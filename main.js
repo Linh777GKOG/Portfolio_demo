ac2a1e1c198d26a4f7cef313543510537c8eb388
Linh Vu <linh777gkog@gmail.com>
2022-07-17 00:37:44 +0700

    add event listener

---
+++ main.js
@@ -0,0 +1,10 @@
+// Menu
+const menu = document.querySelector('.navbar__links');
+const menuButton = document.querySelector('.navbar__icons');
+const overlay = document.querySelector('#overlay');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('navbar__open');
+  menuButton.classList.toggle('open');
+  overlay.classList.toggle('show');
+});
